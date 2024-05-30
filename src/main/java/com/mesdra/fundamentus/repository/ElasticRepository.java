77110db6280b4b93e593420c1ea744e35f3baa81
Vinicius Cavalari <vinicavalari97@gmail.com>
2024-05-30 14:02:16 -0300

    inclusão de autenticação no elastic

--- src/main/java/com/mesdra/fundamentus/repository/ElasticRepository.java
+++ src/main/java/com/mesdra/fundamentus/repository/ElasticRepository.java
@@ -5,9 +5,15 @@ import java.util.List;
 import java.util.Properties;
 
 import org.apache.http.HttpHost;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.apache.http.impl.nio.client.HttpAsyncClientBuilder;
 import org.elasticsearch.action.index.IndexRequest;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestClientBuilder;
 import org.elasticsearch.client.RestHighLevelClient;
 import org.elasticsearch.common.xcontent.XContentType;
 
@@ -38,11 +44,26 @@ public class ElasticRepository {
     }
 
     private RestHighLevelClient createClient() throws PropertiesException {
+
         Properties conf = PropertiesLoader.loadProperties();
         String host = conf.getProperty("elastic.host");
         String port = conf.getProperty("elastic.port");
-        return new RestHighLevelClient(
-                RestClient.builder(new HttpHost(host, Integer.valueOf(port), "http")));
+        String user = conf.getProperty("elastic.user");
+        String pass = conf.getProperty("elastic.pass");
+
+        final CredentialsProvider credentialsProvider = new BasicCredentialsProvider();
+        credentialsProvider.setCredentials(AuthScope.ANY,
+                new UsernamePasswordCredentials(user, pass));
+
+        RestClientBuilder builder = RestClient.builder(new HttpHost(host, Integer.valueOf(port), "http"))
+                .setHttpClientConfigCallback(new RestClientBuilder.HttpClientConfigCallback() {
+                    @Override
+                    public HttpAsyncClientBuilder customizeHttpClient(HttpAsyncClientBuilder httpClientBuilder) {
+                        return httpClientBuilder.setDefaultCredentialsProvider(credentialsProvider);
+                    }
+                });
+
+        return new RestHighLevelClient(builder);
     }
 
 }
